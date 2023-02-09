60e5300f8e9a196ebe3ba4b62df156bd6618499a
fjortegan <fjortegan@gmail.com>
2023-02-09 12:18:34 +0100

    test example completed

--- src/test/java/es/iesrafaelalberti/clasesspring2223/ClasesSpring2223ApplicationTests.java
+++ src/test/java/es/iesrafaelalberti/clasesspring2223/ClasesSpring2223ApplicationTests.java
@@ -10,8 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -31,12 +30,38 @@ class ClasesSpring2223ApplicationTests {
 
 
     @Test
-    void mvcTest() throws Exception {
-        mvc.perform(get("/prisoners/")
-                       .contentType(MediaType.APPLICATION_JSON))
+    void listTest() throws Exception {
+        mvc.perform(get("/prisoners/").contentType(MediaType.APPLICATION_JSON))
                        .andExpect(status().isOk())
-                       .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
+                       .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                       .andExpect(jsonPath("$[0].name").value("bob"))
+                       .andExpect(jsonPath("$[0].age").value(19))
+                       .andExpect(jsonPath("$[0].yearsLeft").value(23));
+    }
 
-                       );
+    @Test
+    void creationTest() throws Exception {
+        // create test prisoner
+        // method post on url /prisoners/
+        // test result
+        // test number of prisoners is correct (+1)
+    }
+    @Test
+    void detailTest() throws Exception {
+        // get test prisoner (method get on url /prisoners/1/)
+        // test result
+    }
+    @Test
+    void updateTest() throws Exception {
+        // get test prisoner (method get on url /prisoners/1/)
+        // modify attributes
+        // method put on url /prisoners/1/
+        // test result
+    }
+    @Test
+    void deleteTest() throws Exception {
+        // method delete on url /prisoners/1/
+        // test result
+        // test number of prisoners is correct (-1)
     }
 }
