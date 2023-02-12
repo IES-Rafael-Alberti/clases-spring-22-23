5c844ebead8dee7426a25234e8e0f4c0d8e15e0a
fjortegan <fjortegan@gmail.com>
2023-02-12 10:30:25 +0100

    test crud

---
+++ src/main/java/es/iesrafaelalberti/clasesspring2223/dto/PrisonerDTO.java
@@ -0,0 +1,16 @@
+package es.iesrafaelalberti.clasesspring2223.dto;
+
+import lombok.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PrisonerDTO implements Serializable {
+    private String name;
+    private Integer age;
+    @NotNull
+    private Integer yearsLeft;
+}
--- src/test/java/es/iesrafaelalberti/clasesspring2223/ClasesSpring2223ApplicationTests.java
+++ src/test/java/es/iesrafaelalberti/clasesspring2223/ClasesSpring2223ApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -25,7 +25,7 @@ class ClasesSpring2223ApplicationTests {
     @Test
     void contextLoads() {
         assert cellRepository.count() == 18;
-        assert prisonerRepository.count() == 7;
+        assert prisonerRepository.count() == 8;
     }
 
 
@@ -41,27 +41,60 @@ class ClasesSpring2223ApplicationTests {
 
     @Test
     void creationTest() throws Exception {
+        // HACK: stores repository count (for execution alone)
+        long prisonerCount = prisonerRepository.count();
         // create test prisoner
+        String testPrisoner = "{\"name\": \"lewis\", \"age\": 24, \"yearsLeft\": 6}";
         // method post on url /prisoners/
-        // test result
+        mvc.perform(post("/prisoners/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(testPrisoner))
+                .andExpect(status().isOk()) // test result
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value("lewis"))
+                .andExpect(jsonPath("$.age").value(24))
+                .andExpect(jsonPath("$.yearsLeft").value(6));
         // test number of prisoners is correct (+1)
+        assert prisonerRepository.count() == prisonerCount + 1;
     }
     @Test
     void detailTest() throws Exception {
         // get test prisoner (method get on url /prisoners/1/)
+        mvc.perform(get("/prisoners/1/").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value("bob"))
+                .andExpect(jsonPath("$.age").value(19))
+                .andExpect(jsonPath("$.yearsLeft").value(23));
         // test result
     }
     @Test
     void updateTest() throws Exception {
         // get test prisoner (method get on url /prisoners/1/)
+        // create test prisoner
+        String testPrisoner = "{\"name\": \"lewis\", \"age\": 24, \"yearsLeft\": 6}";
         // modify attributes
         // method put on url /prisoners/1/
+        mvc.perform(put("/prisoners/1/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(testPrisoner))
+                        .andExpect(status().isOk()) // test result
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("$.name").value("lewis"))
+                        .andExpect(jsonPath("$.age").value(24))
+                        .andExpect(jsonPath("$.yearsLeft").value(6));
         // test result
     }
     @Test
     void deleteTest() throws Exception {
+        // HACK: stores repository count (for execution alone)
+        long prisonerCount = prisonerRepository.count();
         // method delete on url /prisoners/1/
+        mvc.perform(delete("/prisoners/1/").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
         // test result
         // test number of prisoners is correct (-1)
+        assert prisonerRepository.count() == prisonerCount - 1;
     }
 }
