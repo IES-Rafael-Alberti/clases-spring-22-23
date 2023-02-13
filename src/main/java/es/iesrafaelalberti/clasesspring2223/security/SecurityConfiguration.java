4e33687f01f4376d965ab298e0fb15361797a1cf
javier <fjortegan@gmail.com>
2023-02-13 03:56:26 +0100

    security

--- src/main/java/es/iesrafaelalberti/clasesspring2223/security/SecurityConfiguration.java
+++ src/main/java/es/iesrafaelalberti/clasesspring2223/security/SecurityConfiguration.java
@@ -7,9 +7,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfiguration {
 
+/*
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().anyRequest();
     }
+*/
 
 }
