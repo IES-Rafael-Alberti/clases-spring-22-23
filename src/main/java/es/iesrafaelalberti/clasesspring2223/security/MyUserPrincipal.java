91e7eabe47983ca5b95fa885a60950536b8c748c
fjortegan <fjortegan@gmail.com>
2023-02-24 00:36:18 +0100

    security revision

--- src/main/java/es/iesrafaelalberti/clasesspring2223/security/MyUserPrincipal.java
+++ src/main/java/es/iesrafaelalberti/clasesspring2223/security/MyUserPrincipal.java
@@ -25,7 +25,7 @@ public class MyUserPrincipal implements UserDetails {
 
     @Override
     public String getUsername() {
-        return getUsername();
+        return user.getUsername();
     }
 
     @Override
